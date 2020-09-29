94e0df507b0d76100bcb20e33e050442d244f5b7
longdd0101 <longdd.congnghe@gmail.com>
2020-09-30 00:35:36 +0700

    table compare

--- js/animals.js
+++ js/animals.js
@@ -12,10 +12,14 @@ function loadEventListeners() {
   // Create DOM animal List
   fn_createDOMAnimalList();
 
-  // addCompare click
+  // Create table Compare from Local Storage
+  fn_compareLocalStorage();
+
+  // Event add Compare click
   animalList.addEventListener("click", fn_clickCompare);
 
   // Event click "Delete"
+  tableCompare.addEventListener("click", fn_deleteRow);
 }
 
 // Create DOM Animal List =================================================================================
@@ -39,25 +43,88 @@ function fn_createDOMAnimalList() {
   }
 }
 
+// Function create table compare form Local Storage ===================================================================
+function fn_compareLocalStorage() {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    let animalsLocalStorage_Array = fn_getAnimalsLocalStorage();
+    // Detete all existing row
+    while (tableCompare.hasChildNodes()) {
+      tableCompare.removeChild(tableCompare.firstChild);
+    }
+    // create row from local storage
+    for (let animalObjectLocal of animalsLocalStorage_Array) {
+      fn_addTrTag(animalObjectLocal);
+    }
+  }
+}
+
 // Function fn_clickCompare, event click ===================================================================
 function fn_clickCompare(event) {
   if (event.target.classList.contains("addCompare")) {
     const animalDOM = event.target.parentElement;
-
+    const animalID = animalDOM.getAttribute("animalID");
     //Get info animalObject  from DOM
-    const animalObject = fn_getAnimalObject(animalDOM);
-
-    //Add tr to table
-    fn_addTrTag(animalObject);
-
-    console.log(animalObject);
+    const animalObject = fn_getAnimalObjectFromID(animalID);
+    // Change Local Storage
+    fn_setAnimalsLocalStorage(animalObject);
+    // Reload Local Storage and create table compare
+    fn_compareLocalStorage();
   }
 }
 
-// Function get animalObject from animalDOM ===================================================================
-function fn_getAnimalObject(animalDOM) {
-  const animalID = animalDOM.getAttribute("animalID");
+// Delete a row (Tag <tr>) ========================================================================
+function fn_deleteRow(event) {
+  if (event.target.classList.contains("deleteRow")) {
+    // event.target.parentElement.parentElement.remove();
+    const animalDOM = event.target;
+    const animalID = animalDOM.getAttribute("animalID");
+    const animalObject = fn_getAnimalObjectFromID(animalID);
+    fn_removeAnimalsLocalStorage(animalObject);
+    fn_compareLocalStorage();
+  }
+}
 
+// Function get animalsLocalStorage ===================================================================
+function fn_getAnimalsLocalStorage() {
+  let animalsLocalStorage_Array;
+  if (localStorage.getItem("animalsLocalStorage_String") === null) {
+    animalsLocalStorage_Array = [];
+  } else {
+    animalsLocalStorage_Array = JSON.parse(localStorage.getItem("animalsLocalStorage_String"));
+  }
+  return animalsLocalStorage_Array;
+}
+
+// Function set animalLocalStorage ===================================================================
+function fn_setAnimalsLocalStorage(animalObject) {
+  let checkID = 0;
+  let animalsLocalStorage_Array = fn_getAnimalsLocalStorage();
+
+  for (let animalObjectLocal of animalsLocalStorage_Array) {
+    if (animalObjectLocal.id === animalObject.id) {
+      checkID = 1;
+    }
+  }
+  if (checkID === 0) {
+    animalsLocalStorage_Array.push(animalObject);
+  }
+  localStorage.setItem("animalsLocalStorage_String", JSON.stringify(animalsLocalStorage_Array));
+}
+
+// Function remove an Object of animalLocalStorage ===================================================================
+function fn_removeAnimalsLocalStorage(animalObject) {
+  let animalsLocalStorage_Array = fn_getAnimalsLocalStorage();
+
+  for (let animalObjectLocal of animalsLocalStorage_Array) {
+    if (animalObjectLocal.id === animalObject.id) {
+      animalsLocalStorage_Array.splice(animalsLocalStorage_Array.indexOf(animalObjectLocal), 1);
+    }
+  }
+  localStorage.setItem("animalsLocalStorage_String", JSON.stringify(animalsLocalStorage_Array));
+}
+
+// Function get animalObject from animalDOM ===================================================================
+function fn_getAnimalObjectFromID(animalID) {
   if (xhr.readyState === XMLHttpRequest.DONE) {
     const animalsJSON = JSON.parse(xhr.responseText);
 
@@ -72,13 +139,13 @@ function fn_getAnimalObject(animalDOM) {
   return animalObject;
 }
 
-// Add one Tag <tr> to <tbody> ===================================================================
+// Add a Tag <tr> to <tbody> ===================================================================
 function fn_addTrTag(animalObject) {
   // Create HTML Element: tr
   const trTag = document.createElement("tr");
 
   // Add class to <tr> Tag
-  trTag.classList.add("animal-item");
+  trTag.classList.add("compareItem");
 
   // add HTML
   trTag.innerHTML = `  
@@ -88,7 +155,7 @@ function fn_addTrTag(animalObject) {
         <td>${animalObject.name}</td>
         <td>${animalObject.size}</td>
         <td>${animalObject.habitat}</td>
-        <td><button>Delete</button></td>           
+        <td><button class="deleteRow" animalID="${animalObject.id}">Delete</button></td>           
     `;
   // add to tbody
   tableCompare.appendChild(trTag);
